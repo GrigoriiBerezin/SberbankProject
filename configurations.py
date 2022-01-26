e47ef74cb026141f599e0be15d7a2609e450224f
Grigorii Berezin <grigorii_berezin@epam.com>
2022-01-26 10:48:32 +0300

    insert configurations file

---
+++ configurations.py
@@ -0,0 +1,32 @@
+postgresql = {
+    "NAME": "your db name",
+    "USER": "your db user",
+    "PASSWORD": "your db password",
+    "HOST": "your db host",
+    "PORT": "your db port",
+}
+
+vk_api = {
+    "user": "enter your username",
+    "password": "enter your password"
+}
+
+subject_detect = {
+    "max_word_length": 3,
+    "max_nb_words": 1000,
+    "max_seq_length": 50
+}
+
+category_detect = {
+    "stop_words": ["сбербанк", "сбер", "хххх", "го", "заказ", "дня", "сегодня", "штрих", "код", "сбербанка",
+                   "сбермегамаркет", "сбермегамаркета", "сберлогистика", "вместо", "который", "которая", "ПАО", "пао",
+                   "закон", "товар", "россия", "греф", "Греф", "это", "сбер", "любой", "свой", "закон", "плодить", "РФ",
+                   "рф"],
+    "threshold": 0.7
+}
+
+admin = {
+    "error_show_limit": 3
+}
+
+secret_key = "enter your secret key"
